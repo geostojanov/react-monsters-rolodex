10852d653b7ba914bd87d73e98fd3097c5108ff6
George Stojanov <gestojanov@gmail.com>
2020-09-26 16:28:24 +0200

    Updates

---
+++ precache-manifest.4526a2d0a6c38b800b2e1ec807688604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189bc4dc6214a4942d2e9120109e8b81",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00f289e7cccfb57dd103",
+    "url": "/react-monsters-rolodex/static/css/main.d120176d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f289e7cccfb57dd103",
+    "url": "/react-monsters-rolodex/static/js/main.f2eb02b8.chunk.js"
+  },
+  {
+    "revision": "84cac6f49c2db027c893",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.782a98e2.js"
+  }
+]);
